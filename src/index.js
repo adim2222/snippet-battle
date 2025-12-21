3b65cd35c17fcce21ad3417ac1c2693557b8cdd6
adim2222 <nowak.adam2222@protonmail.com>
2025-12-21 15:32:37 +0100

    deleted unnescessary declarations

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
